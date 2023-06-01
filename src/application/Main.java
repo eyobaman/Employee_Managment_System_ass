770ad2af5f0f6dc95df9a78824463affdfc2420d
JakobG569 <132761220+JakobG569@users.noreply.github.com>
2023-06-01 16:18:25 +0300

    changes made see...

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,27 +1,51 @@
 package application;
 	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    
+    private double x = 0;
+    private double y = 0;
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        root.setOnMousePressed((MouseEvent event) ->{
+            x = event.getSceneX(); 
+            y = event.getSceneY();
+        });
+        
+        root.setOnMouseDragged((MouseEvent event) ->{
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+            
+            stage.setOpacity(.8);
+        });
+        
+        root.setOnMouseReleased((MouseEvent event) ->{
+            stage.setOpacity(1);
+        });
+        
+        stage.initStyle(StageStyle.TRANSPARENT);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+   
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
 }
